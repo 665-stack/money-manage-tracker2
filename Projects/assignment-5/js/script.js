09c3a2b28eba201d50b5d1613c779ea822317bcd
unknown <mahmodulshahin665@gmail.com>
2022-03-26 20:18:54 +0600

    add input id

---
+++ Projects/assignment-5/js/script.js
@@ -0,0 +1,6 @@
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputAmountText = inputField.value;
+    const amountValue = parseFloat(inputAmountText);
+    return amountValue;
+}
